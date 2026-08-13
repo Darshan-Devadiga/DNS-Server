c005a435369db3105d3380f036dfc5256b16649d
Darshan <itsdarshan000@gmail.com>
2026-08-13 21:06:12 +0530

    Main file name changed to FileBackedDNSServer

--- src/FileBackedDNSServer.java
+++ src/FileBackedDNSServer.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class FileBackedDNSServer {
     private static final int PORT = 1053;
     private static final Map<String, String> dnsDatabase = new HashMap<>();
 
